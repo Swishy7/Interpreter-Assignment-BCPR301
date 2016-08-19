95cc809bee3bf0ecf18e321909c9d6fe2b74f8ac
Swishy7 <swishy42@gmail.com>
2016-08-19 13:39:10 +1200

    PEP8
    PEP8 formatting

---
+++ Product.py
@@ -0,0 +1,17 @@
+'''
+Created on 19/08/2016
+
+@author: AndrewM
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
