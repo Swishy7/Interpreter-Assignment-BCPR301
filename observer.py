4e2a6adc2c117ed9cc6c5716b56af040c0f7f63b
Swishy7 <swishy42@gmail.com>
2016-10-30 16:08:44 +1300

    Concrete Observer Added
    For products

--- observer.py
+++ observer.py
@@ -5,3 +5,15 @@ Created on 30/10/2016
 '''
 from abc import ABCMeta, abstractmethod
 
+
+class Observer(metaclass=ABCMeta):
+    def __init__(self):
+        self.product_count = 0
+        self.subject = None
+
+    @abstractmethod
+    def update(self, product_count):
+        pass
+
+    def set_subject(self, new_subject):
+        self.subject = new_subject
---
+++ products_observer.py
@@ -0,0 +1,15 @@
+'''
+Created on 30/10/2016
+
+@author: Andrew
+'''
+from InterpreterAssignment.observer import Observer
+
+class ProductObserver(Observer):
+    def __init__(self):
+        self.product_count = 0
+
+    def update(self):
+        self.product_count = len(self.subject.products)
+        print("(> ^_^ )>---[ " + self.product_count + " products are loaded. ]")
+        print(self.name + " updated to " + self.observerState)
